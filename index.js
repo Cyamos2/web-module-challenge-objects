fcf32a20caf02e368dda9d057f257fccdad3d83d
cyamos2 <cyamos2@yahoo.com>
2021-02-03 19:43:39 -0700

    finished tasks 1-4

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    let object = {
+      name: name,
+      price: price,
+      category: category,
+    }
+    return object
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -28,7 +33,12 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
+createMenuItem('macandcheese',5,'lunch');
+createMenuItem('fries',3,"side");
+createMenuItem('mtDew',2,"soda");
+console.log(createMenuItem("macandcheese",5,"lunch"));
+console.log(createMenuItem("fries",3,"side"));
+console.log(createMenuItem("mtDew",2,"soda"));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -48,8 +58,13 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
+  discount: function(string){
+    if (string === 'teacher' || 'student') {
+      return this.price - 4.5
+    } else return this.price - 1.8
+  }
 }
+burger.discount('teacher');
 
 
 
@@ -69,7 +84,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log(reviews[5].feedback);
 
 
 
@@ -79,6 +94,12 @@ Using the reviews array above do the following: (no function needed)
   2. log the whole array to the console, make sure the new review is inside of it   
 */
 
+reviews.push ({
+  name: "Anakin",
+  rating: 3,
+  feedback: "The Jedi poisoned my food, but the restaurant was clean at least!"
+});
+console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -87,7 +108,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
+reviews.splice(7,3, "Reyna", 3.5, "this place is chill with really cool people, great for getting work done on weekdays");
+console.log(reviews);
 
 
 
